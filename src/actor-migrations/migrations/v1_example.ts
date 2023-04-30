2fa220a025e3fe9cb66be78199db1c2e59307198
Juro Oravec <juraj.oravec.josefson@gmail.com>
2023-04-30 19:19:55 +0200

    feat: move migration script to utils package

--- src/actor-migrations/migrations/v1_example.ts
+++ src/actor-migrations/migrations/v1_example.ts
@@ -1,16 +1,25 @@
 import type { ApifyClient } from 'apify-client';
-
-import { loadState, saveState } from '../utils/fs';
+import { Migration, createLocalMigrationState } from 'apify-actor-utils';
+import path from 'path';
 
 // This file is an example of how to make changes to the actors
-// programmatically in a documented way.
-// https://docs.apify.com/academy/getting-started/apify-client
+// programmatically in a documented (and optionally reversible) way.
+//
+// Docs: https://docs.apify.com/academy/getting-started/apify-client
+//
+// Exmaple:
+// migrate - `npm run migrate -- -t v1`
+// unmigrate - `npm run unmigrate -- -t v1`
+
+const { saveState, loadState } = createLocalMigrationState({
+  stateDir: path.join(`${process.cwd()}`, './src/actor-migrations/states'),
+});
 
 /** Run the migration forward */
-export const migrate = async (client: ApifyClient): Promise<void> => {
+const migrate = async (client: ApifyClient): Promise<void> => {
   const actor = client.actor('YOUR_USERNAME/adding-actor');
   // console.log({ actorClient: actor, actorData: await actor.get() });
-
+  console.log('Called MIGRATE with client ', client);
   await saveState(__filename, { get: () => ({} as any) } as any);
 
   // await actor.update({
@@ -23,12 +32,16 @@ export const migrate = async (client: ApifyClient): Promise<void> => {
 };
 
 /** Run the migration backward (AKA undo the migration), if old state is available */
-export const unmigrate = async (client: ApifyClient): Promise<void> => {
+const unmigrate = async (client: ApifyClient): Promise<void> => {
   const actor = client.actor('YOUR_USERNAME/adding-actor');
-
+  console.log('Called UNMIGRATE with client ', client);
   const oldActorState = await loadState(__filename);
 
   // await actor.update({
   //   defaultRunOptions: oldActorState.defaultRunOptions,
   // });
 };
+
+const migration: Migration = { migrate, unmigrate };
+
+export default migration;
